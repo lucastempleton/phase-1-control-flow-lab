b05bdec024940e8a353f1bbef1571dcd37bb3256
Lucas Templeton <templetl99@gmail.com>
2021-11-01 11:46:49 -0500

    commit tree 2643a12364d0fc679a1ae0775a1e8f9cc2d066a1

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return "This one is on me!"
+  }
+  else if (distance > 2000 && distance < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else ;
+    return "No can do."
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  const message = (city === "NYC") ? "Ok, sounds good." : "No go."
+  return message
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case (tip = 'generous'):
+      return "Thank you so much.";
+    case (tip = 'not as generous'):
+      return "Thank you.";
+    default:
+      return "Bye."
+
+  }
 }
